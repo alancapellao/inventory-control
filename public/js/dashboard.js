05816b06dac06444789ac2562cb08c061ad76c87
alancptk7 <alancptk.7@gmail.com>
2023-04-12 09:02:09 -0300

    update dashboard.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -77,20 +77,24 @@ $(function () {
         }
     });
 
-    // Armazenar categoria selecionada
-    const menuDropdown = $("#category");
-    const menuOptions = menuDropdown.find("option");
-    let selectedOption;
-
-    menuDropdown.on("change", function () {
-        selectedOption = $(this).find("option:selected");
-    });
-
     // Função para obter dados dos produtos usando método GET e adicioná-los na página HTML
-    function getProducts() {
+    function getProducts(produto) {
+
+        let url, method;
+
+        // Definindo se é pra renderizar todos os produtos ou produtos específicos
+        if (produto !== 0 && produto !== undefined && produto !== null) {
+            url = `/search`;
+            method = 'POST';
+        } else {
+            url = '/products';
+            method = 'GET';
+        }
+
         $.ajax({
-            url: '/products',
-            method: 'GET',
+            url: url,
+            method: method,
+            data: { search: produto },
             success: function (data) {
                 const produtos = data.produtos;
                 const productsArea = $('.products-column');
@@ -123,7 +127,7 @@ $(function () {
     }
 
     // Renderizando produtos ao carregar página
-    getProducts();
+    getProducts(0);
 
     // Obter informações de um produto ao clicar nele na página
     $('.products-column').on('click', '.products-row', function () {
@@ -194,7 +198,7 @@ $(function () {
                 $(".overlay").hide();
 
                 // Renderizando produtos
-                getProducts();
+                getProducts(0);
 
                 // Limpar o formulário
                 if (form.length) {
@@ -203,7 +207,8 @@ $(function () {
             }
         });
     });
-    // Excluir um produto
+
+    // Excluindo um produto
     $('#delete').on('click', function () {
         // Confirmar com o usuário antes de excluir
         const confirmDelete = confirm('Are you sure you want to delete this product?');
@@ -218,7 +223,7 @@ $(function () {
                     $(".overlay").hide();
 
                     // Renderizando produtos
-                    getProducts();
+                    getProducts(0);
 
                     if (form.length) {
                         form[0].reset();
@@ -230,41 +235,13 @@ $(function () {
             });
         }
     });
+
     // Adicionando um listener ao evento de digitação na barra de busca
     $('.search-bar').on('keyup', function () {
         const searchTerm = $(this).val();
-        // Realizando uma requisição AJAX para a rota "/search"
-        $.ajax({
-            url: '/search',
-            method: 'POST',
-            data: { search: searchTerm },
-            success: function (data) {
-                // Obtendo a lista de produtos retornados pela busca
-                const produtos = data.produtos;
-                const productsArea = $('.products-column');
 
-                productsArea.empty();
-
-                produtos.forEach((produto) => {
-                    const html = `
-                    <div class="products-row" data-product-id="${produto.id}">
-                        <div class="product-cell image">
-                            <span>${produto.item}</span>
-                        </div>
-                        <div class="product-cell category"><span class="cell-label">Category:</span>${produto.category}</div>
-                        <div class="product-cell status-cell">
-                        <span class="cell-label">Status:</span>
-                        <span class="status ${produto.status}">${produto.status}</span>
-                        </div>
-                        <div class="product-cell sales"><span class="cell-label">Sales:</span>${produto.sale}</div>
-                        <div class="product-cell stock"><span class="cell-label">Stock:</span>${produto.stock}</div>
-                        <div class="product-cell price"><span class="cell-label">Price:</span>$ ${produto.price}</div>
-                    </div>
-                `;
-                    productsArea.append(html);
-                });
-            }
-        });
+        // Mandando search para função getProducts
+        getProducts(searchTerm);
     });
 
     //Escondendo e mostrando botão de logout
