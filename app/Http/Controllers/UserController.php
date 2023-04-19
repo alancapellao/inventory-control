fb15a28b98e22fd6c2dba0d9c23a3c6e932a8ca0
alancptk7 <alancptk.7@gmail.com>
2023-04-19 14:58:11 -0300

    update controllers

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -27,31 +27,39 @@ class UserController extends Controller
     {
         $credentials = $request->only('email', 'password');
 
-        if (auth()->attempt($credentials)) {
-            $user = Auth::user()->name;
-            session(['user' => $user]);
+        if (isset($credentials['email']) && !empty($credentials['email']) && isset($credentials['password']) && !empty($credentials['password'])) {
 
-            return response()->json(['error' => false, 'message' => 'Login successful.']);
+            if (auth()->attempt($credentials)) {
+                $user = Auth::user()->name;
+                session(['user' => $user]);
+
+                return response()->json(['error' => false, 'message' => 'Login successful.']);
+            }
+            return response()->json(['error' => true, 'message' => 'Invalid email or password.']);
         }
-        return response()->json(['error' => true, 'message' => 'Invalid email or password.']);
+        return response()->json(['error' => true, 'message' => 'Invalid data.']);
     }
 
     // Função de registro e criptografia
     public function register(Request $request)
     {
-        $data = $request->only(['name', 'email', 'password']);
+        $credentials = $request->only(['name', 'email', 'password']);
 
-        try {
-            $user = User::create([
-                'name' => $data['name'],
-                'email' => strtolower($data['email']),
-                'password' => Hash::make($data['password']),
-            ]);
+        if (isset($credentials['name']) && !empty($credentials['name']) && isset($credentials['email']) && !empty($credentials['email']) && isset($credentials['password']) && !empty($credentials['password'])) {
 
-            return response()->json(['error' => false, 'message' => 'Successfully registered.']);
-        } catch (\Exception $e) {
-            return response()->json(['error' => true, 'message' => 'Email already exists.']);
+            try {
+                $user = User::create([
+                    'name' => $credentials['name'],
+                    'email' => strtolower($credentials['email']),
+                    'password' => Hash::make($credentials['password']),
+                ]);
+
+                return response()->json(['error' => false, 'message' => 'Successfully registered.']);
+            } catch (\Exception $e) {
+                return response()->json(['error' => true, 'message' => 'Email already exists.']);
+            }
         }
+        return response()->json(['error' => true, 'message' => 'Invalid data.']);
     }
 
     // Fechando sessão
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/login', function () {
     return view('login');
-});
+})->name('login');
 
 Route::get('/usuario', [UserController::class, 'getUsuario']);
 Route::post('/login', [UserController::class, 'login']);
